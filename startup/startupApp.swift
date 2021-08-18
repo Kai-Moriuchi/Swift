b31663ca21d80b53dcb3e2647f8d25200b7abc45
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-08-18 20:24:54 +0900

    Initial Commit

---
+++ startup/startupApp.swift
@@ -0,0 +1,17 @@
+//
+//  startupApp.swift
+//  startup
+//
+//  Created by Kai on 2021/08/18.
+//
+
+import SwiftUI
+
+@main
+struct startupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
