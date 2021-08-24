029af35c4de440734d5dc54122228a9160394ac2
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-08-24 16:24:13 +0900

    [Building Lists and Navigation] Section4 & 5終了

--- startup/Model/Landmark.swift
+++ startup/Model/Landmark.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import CoreLocation
 
 // Codable:JSONを自分の好きな型に変換し，アクセスが容易になる
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
---
+++ startup/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  startup
+//
+//  Created by Kai on 2021/08/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
