1910702c3bc9de9b5762da48ea676f5f6cdec32f
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-08-26 19:10:24 +0900

    [fix]

--- startup/Views/LandmarkList.swift
+++ startup/Views/LandmarkList.swift
@@ -23,7 +23,7 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         /* 複数デバイスでのPreviewを可能にする */
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+        ForEach([/*"iPhone SE (2nd generation)", */"iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
