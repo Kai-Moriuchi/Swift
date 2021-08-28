f70ec03e24ed5135dfbff09fa2f22db70a483b8e
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-08-28 19:41:31 +0900

    [Section4] アイコンと背景色を組み合わせる

---
+++ startup/Views/Badges/Badge.swift
@@ -0,0 +1,40 @@
+//
+//  Badge.swift
+//  startup
+//
+//  Created by Kai on 2021/08/28.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+            )
+        }
+        .opacity(0.5)//透過率
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor:  .top)//サイズの設定
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)//配置の設定
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
