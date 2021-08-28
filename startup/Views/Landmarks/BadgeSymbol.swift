343b8d73e742eb4f97d271f3c2849563ea545aa1
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-08-28 19:28:30 +0900

    [Section3]図の模写，色付け，配置

---
+++ startup/Views/Landmarks/BadgeSymbol.swift
@@ -0,0 +1,49 @@
+//
+//  BadgeSymbol.swift
+//  startup
+//
+//  Created by Kai on 2021/08/28.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    static let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height) //図の幅
+                let height = width * 0.75 //図の高さ
+                let spacing = width * 0.030 //下の白部分
+                let middle = width * 0.5 //図の配置
+                let topWidth = width * 0.226 //図の幅
+                let topHeight = height * 0.488 //図の高さ
+                
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+                
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3)) //始点移動
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                ])
+            }
+            .fill(Self.symbolColor)
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
