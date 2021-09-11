f07d11c5c211a05bf52d5823cec31e4e1de53d9a
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-09-11 18:37:06 +0900

    [Section1~3]AppWatch用のApp作成

---
+++ WatchStartup Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchStartup Extension
+//
+//  Created by Kai on 2021/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchStartup Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchStartup Extension
+//
+//  Created by Kai on 2021/09/11.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
