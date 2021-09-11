fb0cf7e9218301b40b0d4f466c6c12f2872e431c
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-09-11 19:20:43 +0900

    [Section1]MacOS用の準備

---
+++ MacStartup/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacStartup
+//
+//  Created by Kai on 2021/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
