d8dcaccbfc9f3ba70a05d4912f18b0350ae3c5f7
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-09-12 18:41:07 +0900

    [Section5]トグルの追加

---
+++ MacStartup/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  startup
+//
+//  Created by Kai on 2021/09/12.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
